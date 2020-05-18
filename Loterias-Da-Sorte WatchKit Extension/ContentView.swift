4a2c025ff673d0e439044576693c2b592a575e19
Gabriel Sória <gabrielss.rock@me.com>
2020-05-17 21:36:46 -0300

    Initial Commit

---
+++ Loterias-Da-Sorte WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Loterias-Da-Sorte WatchKit Extension
+//
+//  Created by Gabriel Soria Souza on 17/05/20.
+//  Copyright © 2020 Gabriel Sória Souza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Loterias-Da-Sorte WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Loterias-Da-Sorte WatchKit Extension
+//
+//  Created by Gabriel Soria Souza on 17/05/20.
+//  Copyright © 2020 Gabriel Sória Souza. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
