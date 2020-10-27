29fcc6e71e30dbe6377fe705357da97c561d1526
Gabriel Sória <gabrielss.rock@me.com>
2020-10-27 00:25:00 -0300

    Adds loading state when loading a new game in the detail game view

--- Loterias-Da-Sorte-iOS/GameDetailView/LotteryGameDetailView.swift
+++ Loterias-Da-Sorte-iOS/GameDetailView/LotteryGameDetailView.swift
@@ -12,18 +12,26 @@ final class GameDetailResult: ObservableObject {
     
     private let service: LotteryNetworkServiceable
     @Published var game: GameDetailModel?
+    private var backupGame: GameDetailModel?
     
     init(service: LotteryNetworkServiceable) {
         self.service = service
     }
     
+    func setBackup(game: GameDetailModel) {
+        self.backupGame = game
+    }
+    
     func getResultsFor(gameNumber: String, and lottery: LotteryGamesNoSpace, completionError: @escaping ((Bool) -> Void)) {
+        self.game = nil
         self.service.request(with: gameNumber, lottery: lottery) { (result: Result<GameDetailModel, NetworkError>) in
             switch result {
             case .success(let game):
                 self.game = game
-            case .failure(let error):
-                print(error)
+            case .failure:
+                if let game = self.backupGame {
+                    self.game = game
+                }
             }
         }
     }
@@ -33,49 +41,44 @@ struct LotteryGameDetailView: View {
     
     @StateObject var gameResults = GameDetailResult(service: LotteryNetworkService(networkService: NetworkService()))
     @State private var alert = false
+    @State private var errorLoadingAlert = false
     var game: GameDetailModel
     var onDisappear: (() -> Void)
     
     var body: some View {
-        
-            if self.gameResults.game == nil {
-                self.initialList.onAppear(perform: {
-                    self.cleanGames()
-                    UITableViewCell.appearance().selectionStyle = .none
-                }).navigationBarItems(trailing: Button(action: {
-                    self.alert.toggle()
-                }, label: {
-                    Image(systemName: "magnifyingglass.circle.fill")
-                })).actionSheet(isPresented: self.$alert) {
-                    ActionSheet(title: Text("Deseja ver algum jogo específico?"),
-                                message: Text("Toque em procurar e digite o número do jogo que você deseja ver"),
-                                buttons: [.default(Text("Procurar"),
-                                                   action: {
-                                                    self.showAlertWithTextField(with: "Digite o número do jogo que você deseja ver", message: nil, placeholder: "Número do jogo", actionTitle: "Procurar", cancelTitle: "Voltar") { number in
-                                                        self.searchNewGame(with: number)
-                                                    }
-                                                   }), .cancel(Text("Cancelar"))])
-                }
+        Group {
+            if self.gameResults.game != nil {
+                self.newList
             } else {
-                self.newList.onAppear(perform: {
-                    self.cleanGames()
-                    UITableViewCell.appearance().selectionStyle = .none
-                }).navigationBarItems(trailing: Button(action: {
-                    self.alert.toggle()
-                }, label: {
-                    Image(systemName: "magnifyingglass.circle.fill")
-                })).actionSheet(isPresented: self.$alert) {
-                    ActionSheet(title: Text("Deseja ver algum jogo específico?"),
-                                message: Text("Toque em procurar e digite o número do jogo que você deseja ver"),
-                                buttons: [.default(Text("Procurar"),
-                                                   action: {
-                                                    self.showAlertWithTextField(with: "Digite o número do jogo que você deseja ver", message: nil, placeholder: "Número do jogo", actionTitle: "Procurar", cancelTitle: "Voltar") { number in
-                                                        self.searchNewGame(with: number)
-                                                    }
-                                                   }), .cancel(Text("Cancelar"))])
-                }
+                ProgressView()
+                    .scaleEffect(1.5)
+                    .progressViewStyle(CircularProgressViewStyle())
             }
-        
+        }.onAppear(perform: {
+            self.gameResults.setBackup(game: self.game)
+            self.searchNewGame(with: game.gameData.concourseNumber)
+            self.cleanGames()
+            UITableViewCell.appearance().selectionStyle = .none
+        }).navigationBarItems(trailing: Button(action: {
+            self.alert.toggle()
+        }, label: {
+            Image(systemName: "magnifyingglass.circle.fill")
+        })).actionSheet(isPresented: self.$alert) {
+            ActionSheet(title: Text("Deseja ver algum jogo específico?"),
+                        message: Text("Toque em procurar e digite o número do jogo que você deseja ver"),
+                        buttons: [.default(Text("Procurar"),
+                                           action: {
+                                            self.showAlertWithTextField(with: "Digite o número do jogo que você deseja ver", message: nil, placeholder: "Número do jogo", actionTitle: "Procurar", cancelTitle: "Voltar") { number in
+                                                self.searchNewGame(with: number)
+                                            }
+                                           }), .cancel(Text("Cancelar"))])
+        }.alert(isPresented: self.$errorLoadingAlert, content: {
+            Alert(title: Text("Ops!"),
+                  message: Text("Erro ao carregar o jogo, tente novamente!"),
+                  primaryButton: .default(Text("Tentar novamente"), action: {
+                self.searchNewGame(with: self.gameResults.game?.gameData.concourseNumber ?? "")
+            }), secondaryButton: .default(Text("Cancelar")))
+        })
     }
     
     var newList: some View {
@@ -91,22 +94,11 @@ struct LotteryGameDetailView: View {
         }
     }
     
-    var initialList: some View {
-        List {
-            ForEach(self.game.gameDetailContent) { item in
-                VStack(alignment: .leading, spacing: 10) {
-                    Text(item.title).font(.title)
-                    Text(item.content).font(.title2)
-                }.foregroundColor(self.game.gameData.lotteryGame
-                                    .colorFromGame().newColor)
-            }
-            .navigationBarTitle(game.gameData.lotteryGame.rawValue, displayMode: .automatic)
-        }
-    }
-    
     private func searchNewGame(with number: String) {
         self.gameResults.getResultsFor(gameNumber: number, and: self.game.gameData.lotteryGameNoSpace, completionError: { error in
-            print(error)
+            if error {
+                self.errorLoadingAlert = true
+            }
         })
     }
     
