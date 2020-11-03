9f1494431800db38fa002657b8cd97c00178d9a8
Gabriel Sória <gabrielss.rock@me.com>
2020-11-02 22:52:40 -0300

    Adds app clip to the project

--- Loterias-Da-Sorte-iOS/SettingsView/SettingsRowActions.swift
+++ Loterias-Da-Sorte-iOS/SettingsView/SettingsRowActions.swift
@@ -20,7 +20,7 @@ final class SettingsRowActions {
     }
     
     func openAbout() {
-        guard let url = URL(string: "https://www.loteriasdasorte.app/sobre") else { return }
+        guard let url = URL(string: "https://www.loteriasdasorte.app/") else { return }
         UIApplication.shared.open(url)
     }
     
@@ -35,12 +35,12 @@ final class SettingsRowActions {
     }
     
     func openPrivacy() {
-        guard let url = URL(string: "https://www.loteriasdasorte.app/politica-de-privacidade") else { return }
+        guard let url = URL(string: "https://www.loteriasdasorte.app/") else { return }
         UIApplication.shared.open(url)
     }
     
     func openTerms() {
-        guard let url = URL(string: "https://www.loteriasdasorte.app/termos-de-uso") else { return }
+        guard let url = URL(string: "https://www.loteriasdasorte.app/") else { return }
         UIApplication.shared.open(url)
     }
 }
